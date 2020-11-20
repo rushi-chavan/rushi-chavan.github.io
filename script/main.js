7698a0f2c927017407d8c2eb124d07bfea33a7a9
root <root@LENOVO-IP520.localdomain>
2020-11-20 18:32:51 +0530

    Home initialised

---
+++ script/main.js
@@ -0,0 +1,11 @@
+// window.onload = printLength();
+// function printLength() {
+//   console.log("Hello");
+//   let paths = document.querySelectorAll("path");
+//   let array = new Array();
+//   paths.forEach((e) => {
+//     console.log(e.getTotalLength());
+//     array.push(e.getTotalLength());
+//   });
+//   console.log("array", array);
+// }
